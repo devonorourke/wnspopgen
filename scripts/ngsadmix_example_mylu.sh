a3a863064abea3197be67a00e21214c5f03e3c59
Devon O'Rourke <devon.orourke@gmail.com>
2020-02-27 14:34:13 -0500

    Update ngsadmix_example_mylu.sh

--- scripts/ngsadmix_example_mylu.sh
+++ scripts/ngsadmix_example_mylu.sh
@@ -1,12 +1,23 @@
 ## example script for analyzing the M. lucifugus SNP sites common among both populations
-## note that the same script was applied for K=2, K=3, and K=4 to generate the data used in the figure produced
 
 NGSADMIX=/projects/foster_lab/pkgs/angsd/misc/NGSadmix
 BEAGLEFILE=/scratch/dro49/myluwork/popgen/angsd/mylu/LUcommon.beagle.gz
 OUTDIR=$(pwd)
 
 $NGSADMIX \
--P $SLURM_CPUS_PER_TASK \
+-P $SLURM_CPUS_PER_TASK \   ## we use a Slurm program manager to submit jobs; change this to an integer for # thread you want to use in your own run
 -likes $BEAGLEFILE \
 -o LUcommon_k2 \
 -K 2 -minMaf 0.05 -seed 1
+
+$NGSADMIX \
+-P $SLURM_CPUS_PER_TASK \
+-likes $BEAGLEFILE \
+-o LUcommon_k3 \
+-K 3 -minMaf 0.05 -seed 1
+
+$NGSADMIX \
+-P $SLURM_CPUS_PER_TASK \
+-likes $BEAGLEFILE \
+-o LUcommon_k4 \
+-K 4 -minMaf 0.05 -seed 1
