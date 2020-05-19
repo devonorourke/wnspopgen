cd2bf6892ddfbb456d0881bfeeb1be4a1c48156d
Devon O'Rourke <devon.orourke@gmail.com>
2020-05-19 10:46:41 -0400

    Update pcadapt_LUallSamps.R

--- scripts/pcadapt_LUallSamps.R
+++ scripts/pcadapt_LUallSamps.R
@@ -8,6 +8,18 @@ library(ggrepel)
 library(ggpubr)
 library(qvalue)
 
+## install theme for plot
+theme_devon <- function () { 
+  theme_bw(base_size=12, base_family="Avenir") %+replace% 
+    theme(
+      panel.background  = element_blank(),
+      plot.background = element_rect(fill="transparent", colour=NA), 
+      legend.background = element_rect(fill="transparent", colour=NA),
+      legend.key = element_rect(fill="transparent", colour=NA)
+    )
+}
+
+
 ## get names of samples
 pop_info <- read_delim("https://raw.githubusercontent.com/devonorourke/wnspopgen/master/data/PLINK/mylu_proper.fam", 
                        col_names = FALSE, delim = "\t") %>% 
@@ -15,17 +27,20 @@ pop_info <- read_delim("https://raw.githubusercontent.com/devonorourke/wnspopgen
   select(Pop, Indiv) %>% 
   mutate(PopNames=paste(Pop, Indiv, sep="-"))
 
-## download VCF file from OSF site: https://osf.io/z2e74/download
-vcfPath <- "LUall_pruned.vcf.gz"
+## download VCF file from OSF site - see: https://osf.io/97mc6/
+vcfPath <- "LUcommon_maf05_noRepeats_plinkLD_filtd.vcf.gz"
 fullvcf <- read.pcadapt(vcfPath, type = "vcf")
 
 ## Screeplot to check for K values to input
 allsites.list.k10 <- pcadapt(input = fullvcf, K = 10, min.maf = 0.05)
-plot(allsites.list.k10, option = "screeplot")
-  ## looks like just 3 PC's are sufficient for most variance to be captured
+plot(allsites.list.k10, option = "screeplot") + theme_devon()
+## save plot as 'screeplot_k10_LU'; export at w600 x h610
+  ## looks like just 2 PC's are sufficient for most variance to be captured; will gather 3 for test
+  ## note the drop in proportion of varance is steep, but the absolute difference is REALLY small.
 
 ## Import data with K=3
 allsites.list.k3 <- pcadapt(input = fullvcf, K = 3, min.maf = 0.05)
+rm(allsites.list.k10)
 
 ## collect data from this file:
 scores_dat <- data.frame(allsites.list.k3$scores) %>% 
@@ -37,29 +52,38 @@ minval_pc <- min(scores_dat$PC1, scores_dat$PC2, scores_dat$PC3) - 0.05
 
 ## plot PC1-PC2
 pc12 <- ggplot(scores_dat,
-       aes(x=PC1, y=PC2, color=Pop, label=Indiv)) +
-  geom_point() +
+       aes(x=PC1, y=PC2, color=Pop, label=Indiv, shape=Pop)) +
+  geom_point(alpha=0.5) +
   geom_text_repel(data = scores_dat %>% filter(PC2 < -0.2 | PC1 > 0.5)) +
   scale_x_continuous(limits = c(minval_pc, maxval_pc)) +
-  scale_y_continuous(limits = c(minval_pc, maxval_pc))
-
+  scale_y_continuous(limits = c(minval_pc, maxval_pc)) +
+  scale_color_manual(values = c("brown", "blue")) +
+  theme_devon()
+  
 ## plot PC1-PC3
 pc13 <- ggplot(scores_dat,
-       aes(x=PC1, y=PC3, color=Pop, label=Indiv)) +
-  geom_point() +
-  geom_text_repel(data = scores_dat %>% filter(PC3 > 0.5 | PC3 < -0.08)) +
+       aes(x=PC1, y=PC3, color=Pop, label=Indiv, shape=Pop)) +
+  geom_point(alpha=0.5) +
+  geom_text_repel(data = scores_dat %>% filter(PC3 > 0.4)) +
   geom_text_repel(data = scores_dat %>% filter(PC1 > 0.5)) + 
   scale_x_continuous(limits = c(minval_pc, maxval_pc)) +
-  scale_y_continuous(limits = c(minval_pc, maxval_pc))
+  scale_y_continuous(limits = c(minval_pc, maxval_pc)) +
+  scale_color_manual(values = c("brown", "blue")) +
+  theme_devon()
 
 ## plot PC2-PC3
 pc23 <- ggplot(scores_dat,
-       aes(x=PC2, y=PC3, color=Pop, label=Indiv)) +
-  geom_point() +
-  geom_text_repel(data = scores_dat %>% filter(PC3 > 0.5 | PC3 < -0.08)) +
+       aes(x=PC2, y=PC3, color=Pop, label=Indiv, shape=Pop)) +
+  geom_point(alpha=0.5) +
+  geom_text_repel(data = scores_dat %>% filter(PC2 < -0.5)) +
+  geom_text_repel(data = scores_dat %>% filter(PC3 > 0.5)) +
   scale_x_continuous(limits = c(-0.8, 0.8)) +
-  scale_y_continuous(limits = c(-0.8, 0.8))
+  scale_y_continuous(limits = c(-0.8, 0.8)) +
+  scale_color_manual(values = c("brown", "blue")) +
+  theme_devon()
 
 ## stitch together:
-ggarrange(pc12, pc13, pc23, 
-          common.legend = TRUE, ncol = 3, nrow=1)
+ggarrange(pc12, NULL, pc13, pc23, 
+          common.legend = TRUE, ncol = 2, nrow=2)
+
+## save as 'pcadapt_PCA_LU_allSamps'; export at w600 x h600
